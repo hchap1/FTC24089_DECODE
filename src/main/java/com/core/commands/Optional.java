e3121fc1dbb1a39d7242bfd68d169d042988ad7f
Harrison Chapman <hchap1@hotmail.com>
2025-08-20 09:24:35 +1000

    feat: optional command

---
+++ src/main/java/com/core/commands/Optional.java
@@ -0,0 +1,40 @@
+package com.core.commands;
+
+import java.util.function.BooleanSupplier;
+
+public class Optional extends TaskBase {
+
+    private TaskBase task;
+    private BooleanSupplier condition;
+    private boolean evaluated;
+
+    public Optional(TaskBase task, BooleanSupplier condition) {
+        this.task = task;
+        this.condition = condition;
+        this.evaluated = false;
+    }
+
+    @Override
+    public void init() {
+        this.evaluated = this.condition.getAsBoolean();
+        if (this.evaluated) this.task.init();
+    }
+
+    @Override
+    public void run() {
+        if (this.evaluated) this.task.run();
+    }
+
+    @Override
+    public boolean finished() {
+        if (!this.evaluated) return true;
+        return this.task.finished();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if (this.evaluated && interrupted) {
+            this.task.end(true);
+        }
+    }
+}
