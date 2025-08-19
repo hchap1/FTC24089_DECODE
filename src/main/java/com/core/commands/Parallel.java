6aa311a603eec30b05b3abbb7aa8c0f9ca344622
Harrison Chapman <hchap1@hotmail.com>
2025-08-20 09:03:52 +1000

    fix: check whteher a task has ended once, and if so, never execute again

--- src/main/java/com/core/commands/Parallel.java
+++ src/main/java/com/core/commands/Parallel.java
@@ -7,10 +7,16 @@ import java.util.List;
 public class Parallel extends TaskBase {
 
     private List<TaskBase> tasks;
+    private List<Boolean> finished;
 
     public Parallel(TaskBase... tasks) {
         this.tasks = new ArrayList<>();
+        this.finished = new ArrayList<>();
         this.tasks.addAll(Arrays.asList(tasks));
+
+        for (int i = 0; i < this.tasks.size(); i++) {
+            this.finished.add(false);
+        }
     }
 
     @Override
@@ -18,24 +24,37 @@ public class Parallel extends TaskBase {
 
     @Override
     public void run() {
-        for (TaskBase task : this.tasks) {
-            task.run();
-            if (task.finished()) {
-                task.end(false);
+        // For each task (and its finished status)
+        for (int i = 0; i < this.tasks.size(); i++) {
+
+            // If the task has finished once before, never execute again
+            if (this.finished.get(i)) {
+                continue;
+            }
+
+            // Run the task
+            this.tasks.get(i).run();
+
+            // If the task has finished, mark it as such (and end it)
+            if (this.tasks.get(i).finished()) {
+                this.tasks.get(i).end(false);
+                this.finished.set(i, true);
             }
         }
     }
 
     @Override
     public boolean finished() {
-        for (TaskBase task : this.tasks) {
-            if (!task.finished()) return false;
+        // Return true if all tasks have finished
+        for (Boolean finished : this.finished) {
+            if (!finished) return false;
         }
         return true;
     }
 
     @Override
     public void end(boolean interrupted) {
+        // Force end all tasks on interruption
         if (interrupted) {
             for (TaskBase task : this.tasks) {
                 task.end(true);
--- src/main/java/com/core/commands/Series.java
+++ src/main/java/com/core/commands/Series.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Series extends TaskBase {
-
     private List<TaskBase> tasks;
     private int index;
 
